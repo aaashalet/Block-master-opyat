6a4e0686f1aa45e308c240e32bd10c503d9bfea6
ashalet <soulboy228@gmail.com>
2025-02-01 18:26:19 +0700

    checking
    некая проверочка

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Block Master")
 
 running = True
+
+GRID_SIZE = 10
+CELL_SIZE = WIDTH // GRID_SIZE
+GRID_COLOR = (50, 50, 50)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
